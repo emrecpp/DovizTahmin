5ce271b738fb2c66d392e0126daa98552f48bad5
Emre <emrecpp1@gmail.com>
2024-05-19 22:27:31 +0300

    qt & model

--- predict.py
+++ predict.py
@@ -1,27 +1,30 @@
 import os
-
 import pandas as pd
 import numpy as np
-import tensorflow as tf
 from datetime import datetime, timedelta
+from enum import IntEnum
+
+class DolarOrEuro(IntEnum):
+    DOLAR = 1
+    EURO = 2
 
 PROJECT_TOP_DIR = os.getcwd() + os.sep + ".." + os.sep
 PATH_DATASET_DIR = PROJECT_TOP_DIR + "dataset" + os.sep
 PATH_MODEL_DIR = PROJECT_TOP_DIR + "model_experiments" + os.sep
-def predict_dollar_price(date):
+def predict_dollar_or_euro_price(dolarOrEuro:DolarOrEuro, date:datetime):
     if not isinstance(date, datetime):
         raise ValueError("Input should be a datetime object")
 
-    df = pd.read_excel(PATH_DATASET_DIR+'Dolar.xlsx')
+    dolar_or_euro:str = "Dolar" if dolarOrEuro == DolarOrEuro.DOLAR else "Euro"
+    df = pd.read_excel(f"{PATH_DATASET_DIR}{dolar_or_euro}.xlsx")
 
     df['Tarih'] = pd.to_datetime(df['Tarih'], dayfirst=True)
     df.set_index('Tarih', inplace=True)
-    df['Dolar'] = df['Dolar'].interpolate(method='linear')
-    df["Dolar"] = df["Dolar"].astype("float32")
+    df[dolar_or_euro] = df[dolar_or_euro].interpolate(method='linear')
+    df[dolar_or_euro] = df[dolar_or_euro].astype("float32")
     df.dropna(inplace=True)
 
     WINDOW_SIZE = 30  # Adjust this to match the model's expected input size
-    HORIZON = 1
 
     start_date = df.index.min()
     end_date = df.index.max()
@@ -29,16 +32,18 @@ def predict_dollar_price(date):
     if date <= end_date:
         raise ValueError("The requested date should be in the future compared to the dataset")
 
+    import tensorflow as tf
+
     # Load the model
-    model = tf.keras.models.load_model(PATH_MODEL_DIR+"model_2_dense/")
+    model = tf.keras.models.load_model(PATH_MODEL_DIR + dolar_or_euro + os.sep +"model_2_dense/")
 
     # Initialize the last known window prices from the dataset
     last_known_date = df.index[-1]
-    window_prices = df['Dolar'].iloc[-WINDOW_SIZE:].values
+    window_prices = df[dolar_or_euro].iloc[-WINDOW_SIZE:].values
 
     # Predict iteratively up to the requested date
     current_date = last_known_date + timedelta(days=1)
-    while current_date <= date:
+    while current_date < date:
         window_prices = np.expand_dims(window_prices, axis=0)  # Shape: (1, 30)
         prediction = model.predict(window_prices)
         predicted_price = prediction[0][0]
